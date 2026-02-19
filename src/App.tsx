3e86798863fbf860b7a69b0669d4fa5d21e81ab8
Aleksandra <tapokstayl@gmail.com>
2026-02-20 03:26:48 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import { RouterProvider } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
-import { router } from './router.tsx'
+import MainPage from './pages/MainPage.tsx'
+import NotFoundPage from './pages/NotFoundPage.tsx'
+import ProgramPage from './pages/ProgramPage.tsx'
 
 export default function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/program" element={<ProgramPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </HashRouter>
+  )
 }
